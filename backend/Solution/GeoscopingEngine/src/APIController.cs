53c3e9505d29373cac4723d88ea548f8d5c7d5ad
Ethan Rule <94942533+EthanRule@users.noreply.github.com>
2025-04-04 16:32:10 -0700

    init

--- backend/Solution/GeoscopingEngine/src/APIController.cs
+++ backend/Solution/GeoscopingEngine/src/APIController.cs
@@ -1,6 +1,23 @@
-﻿namespace GeoscopingEngine.src
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace GeoscopingEngine.Src
 {
+    /// <summary>
+    /// APIController serves as a bridge between the API and the underlying geoscoping engine.
+    /// </summary>
     public class APIController
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="APIController"/> class.
+        /// </summary>
+        public APIController()
+        {
+
+        }
+
+        public void HandleRequest(HttpRequest request)
+        {
+
+        }
     }
 }
