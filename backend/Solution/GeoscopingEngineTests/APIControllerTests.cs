f69c3935b1a543f4e146efb05f6f1edd63386bee
Ethan Rule <94942533+EthanRule@users.noreply.github.com>
2025-04-07 10:06:41 -0700

    Initial test methods for the APIController class

---
+++ backend/Solution/GeoscopingEngineTests/APIControllerTests.cs
@@ -0,0 +1,46 @@
+﻿namespace GeoscopingEngineTests
+{
+    using Xunit;
+
+    /// <summary>
+    /// Tests for the API Controller.
+    /// </summary>
+    public class APIControllerTests
+    {
+        /// <summary>
+        /// Initial API Controller FormatResponse() test method.
+        /// </summary>
+        [Fact]
+        public void TestFormatResponse()
+        {
+            Assert.True(true);
+        }
+
+        /// <summary>
+        /// Initial API Controller HandleRequest() test method.
+        /// </summary>
+        [Fact]
+        public void TestHandleRequest()
+        {
+            Assert.True(true);
+        }
+
+        /// <summary>
+        /// Initial API Controller RouteToService() test method.
+        /// </summary>
+        [Fact]
+        public void TestRouteToService()
+        {
+            Assert.True(true);
+        }
+
+        /// <summary>
+        /// Initial API Controller RouteToEventController() test method.
+        /// </summary>
+        [Fact]
+        public void TestRouteToEventService()
+        {
+            Assert.True(true);
+        }
+    }
+}
