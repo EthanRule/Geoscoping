c1fa7ad317b50503ec5c0c7386d3fea07115a5e2
Ethan Rule <94942533+EthanRule@users.noreply.github.com>
2025-04-10 13:37:24 -0700

    Added images to Events page

--- frontend/geoscoping/app/about/page.tsx
+++ frontend/geoscoping/app/about/page.tsx
@@ -1,3 +1,8 @@
 export default async function About() {
-  return <div>Hello From About</div>;
+  return (
+    <div>
+      Sup guys xaryuuuuuuu here and welcome to another guide on playing frost
+      mage
+    </div>
+  );
 }
